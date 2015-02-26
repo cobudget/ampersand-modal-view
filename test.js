009927453650e44caddb273ac03770ab992c1c59
Michael Williams <dinosaur@riseup.net>
2015-02-26 16:59:05 +1300

    add basic test for existing functionality

--- test.js
+++ test.js
@@ -1,9 +1,33 @@
 var test = require('tape');
+var View = require('ampersand-view');
+var select = require('dom-select');
 
 var ModalView;
 
 test("should require module", function (t) {
   ModalView = require('./');
   t.ok(ModalView);
+  t.end();
+});
+
+test("should open simple modal in body", function (t) {
+  var ContentView = View.extend({
+    autoRender: true,
+    template: [
+      '<div class="content-view-test">',
+        'content',
+      '</div>',
+    ].join(''),
+  });
+  var contentView = new ContentView();
+  var modal = new ModalView({
+    title: "Test Modal",
+    description: "this is only a test",
+    contentView: contentView,
+  });
+  modal.openIn('body');
+
+  t.equal(modal.el, select('body > .modal-overlay'));
+  t.equal(contentView.el, select('[data-hook="content"] > .content-view-test'))
   t.end();
 });
